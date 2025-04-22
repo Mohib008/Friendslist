ace0f43663ff14e255c35641ad4223ecd661813a
mohib <mohib.0008@gmail.com>
2025-04-22 15:14:47 -0400

    new useState and useEffect added to app components

--- Components/app.jsx
+++ Components/app.jsx
@@ -1,13 +1,64 @@
 import React from "react";
 
-function app() {
+import { useState, useEffect } from "react";
+
+export default function SaveButton() {
+  const [isOnline, setIsOnline] = useState(true);
+  const [isOffline, setOffline] = useState(false);
+  useEffect(() => {
+    function handleOnline() {
+      setIsOnline(true);
+    }
+    function handleOffline() {
+      setIsOnline(false);
+    }
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  function handleSaveClick() {
+    console.log("✅ Progress saved");
+  }
+  function handlOffClick() {
+    console.log("❌ Progress not saved");
+  }
+  // Simulate an API call to save progress
+  function saveProgress() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve("Progress saved");
+      }, 2000);
+    });
+  }
+  // Simulate an API call to save progress
+  function saveProgress() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve("Progress saved");
+      }, 2000);
+    });
+  }
+
   return (
     <>
-      <div className="grid grid-cols-1 gap-4 relative h-screen w-screen bg-gray-100">
-        This is the app component!
-      </div>
+      <button
+        className=" hover:bg-blue-400 bg-blue-300 hover:text-white shadow-2xl bg-shadow-black p-4 transition-600 transform-3d"
+        disabled={!isOnline}
+        onClick={handleSaveClick}
+      >
+        {isOnline ? "Save progress" : "Reconnecting..."}
+      </button>
+      <button
+        className=" hover:bg-blue-400 bg-blue-300 hover:text-white shadow-2xl bg-shadow-black p-4 transition-600 transform-3d"
+        disabled={!isOffline}
+        onClick={handlOffClick}
+      >
+        {isOffline ? " Progress not Saved" : "❌ Offline..."}
+      </button>
     </>
   );
 }
-
-export default app;
