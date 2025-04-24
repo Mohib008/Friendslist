6adf3fb54e838acb17ea114a145cd644b00dd20f
mohib <mohib.0008@gmail.com>
2025-04-24 16:19:54 -0400

    new hooks added to footer component

--- Components/footer.jsx
+++ Components/footer.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
 function footer() {
-  return <div>footer</div>;
+  const [count, setCount] = useState(0);
+
+  const changeCount = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <p>Number Count is: {count} </p>
+      <button
+        className="bg-blue-400 hover:bg-blue-500 rounded-xl p-3  hover:text-white font-bold"
+        onClick={changeCount}
+      >
+        Increment
+      </button>
+    </div>
+  );
 }
 
 export default footer;
