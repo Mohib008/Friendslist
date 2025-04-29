10d8e8b64b13a9eacd0e73f5f4181a5d8b629a85
mohib <mohib.0008@gmail.com>
2025-04-29 13:40:33 -0400

    update

--- Components/footer.jsx
+++ Components/footer.jsx
@@ -3,20 +3,46 @@ import { useState } from "react";
 
 function footer() {
   const [count, setCount] = useState(0);
+  const [isHappy, setIsHappy] = useState(true);
 
-  const changeCount = () => {
+  const incrementCount = () => {
     setCount(count + 1);
   };
+  const decreamntCount = () => {
+    setCount(count - 1);
+  };
+  const restCount = () => {
+    setCount(0);
+  };
+  const toggleIsHappy = () => {
+    setIsHappy(!isHappy);
+  };
   return (
-    <div>
-      <p>Number Count is: {count} </p>
-      <button
-        className="bg-blue-400 hover:bg-blue-500 rounded-xl p-3  hover:text-white font-bold"
-        onClick={changeCount}
-      >
-        Increment
-      </button>
-    </div>
+    <>
+      <div>
+        <p
+          className="text-center text-2xl size-10 font-bold"
+          onClick={toggleIsHappy}
+        >
+          {isHappy ? "🙋" : "😥"}
+        </p>
+        <p>Number Count is: {count} </p>
+        <div className="flex justify-center gap-4">
+          <button
+            className="bg-blue-400 hover:bg-blue-500 rounded-xl p-3  hover:text-white font-bold"
+            onClick={incrementCount}
+          >
+            Increment
+          </button>
+          <button className="bg-blue-400 hover:bg-blue-500 rounded-xl p-3  hover:text-white font-bold">
+            Decrement
+          </button>
+          <button className="bg-blue-400 hover:bg-blue-500 rounded-xl p-3  hover:text-white font-bold">
+            Reset
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
